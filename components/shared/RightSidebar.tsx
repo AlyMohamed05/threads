080389256489a353bb0211afe316f9f2ace40440
Aly <aly.mohamed.3313@gmail.com>
2024-01-24 05:37:57 +0200

    app shell

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,5 +1,16 @@
 function RightSidebar() {
-  return <h1>RightSide Bar</h1>;
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-col flex-1 justify-start">
+        <h3 className="text-heading3-bold text-light-1">
+          Suggested Communities
+        </h3>
+      </div>
+      <div className="flex flex-col flex-1 justify-start">
+        <h3 className="text-heading3-bold text-light-1">Suggested Users</h3>
+      </div>
+    </section>
+  );
 }
 
 export default RightSidebar;
